789ba00a932116a2f416c6e25cfb901384056ffa
Filipe Almeida <filipe@filipe>
2024-10-15 14:13:07 -0300

    Fixing provider version [apply-homolog] & [apply-prod]

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,10 +3,11 @@
 # ---------------------------------------------------------------------------------------------------------------------
 terraform {
   required_version = "~>0.13.4"
+  
   required_providers {
    aws = {
       source = "hashicorp/aws"
-      version = "5.0"
+      version = "~> 5.0"
     } 
   }
 
